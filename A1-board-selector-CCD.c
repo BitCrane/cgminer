f121941824c7c03f51a23f54d1d21dbd905f59b7
Mark.Z <magadeath@gmail.com>
2015-04-01 18:00:46 +0800

    Update A1-board-selector-CCD.c

--- A1-board-selector-CCD.c
+++ A1-board-selector-CCD.c
@@ -26,66 +26,69 @@
 #include "miner.h"
 
 #include "A1-board-selector.h"
-#include "i2c-context.h"
 
 static struct board_selector ccd_selector;
 
-struct i2c_ctx *U1_tca9535;
-uint8_t chain_mask = 0xff;
-uint8_t active_chain = 255;
-pthread_mutex_t lock;
+struct ccd_ctx {
+	struct i2c_ctx U1_tca9535;
+	uint8_t board_mask;
+	uint8_t active_board;
+	pthread_mutex_t lock;
+};
 
+static struct ccd_ctx ccd_ctx = {
+	.U1_tca9535 = { .addr = 0x27, .file = -1 },
+	.board_mask = 0xff,
+	.active_board = 255,
+};
 
 #define UNUSED_BITS 0xe0
 
 static void ccd_unlock(void)
 {
-	mutex_unlock(&lock);
+	mutex_unlock(&ccd_ctx.lock);
 }
 
 static void ccd_exit(void)
 {
-	if (U1_tca9535 != NULL)
-		U1_tca9535->exit(U1_tca9535);
+	i2c_slave_close(&ccd_ctx.U1_tca9535);
 }
-uint8_t retval = 0;
 
 extern struct board_selector *ccd_board_selector_init(void)
 {
-	mutex_init(&lock);
-	U1_tca9535 = i2c_slave_open(I2C_BUS, 0x27);
-	if (U1_tca9535 == NULL)
-		return NULL;
-	bool retval =	U1_tca9535->write(U1_tca9535, 0x06, 0xe0) &&
-			U1_tca9535->write(U1_tca9535, 0x07, 0xe0) &&
-			U1_tca9535->write(U1_tca9535, 0x02, 0x1f) &&
-			U1_tca9535->write(U1_tca9535, 0x03, 0x00);
+	mutex_init(&ccd_ctx.lock);
+	struct i2c_ctx *ctx = &ccd_ctx.U1_tca9535;
+	bool retval =	i2c_slave_open(ctx, I2C_BUS) &&
+			i2c_slave_write(ctx, 0x06, 0xe0) &&
+			i2c_slave_write(ctx, 0x07, 0xe0) &&
+			i2c_slave_write(ctx, 0x02, 0x1f) &&
+			i2c_slave_write(ctx, 0x03, 0x00);
 	if (retval)
 		return &ccd_selector;
 	ccd_exit();
 	return NULL;
 }
 
-static bool ccd_select(uint8_t chain)
+static bool ccd_select(uint8_t board)
 {
-	if (chain >= CCD_MAX_CHAINS)
+	if (board >= CCD_MAX_CHAINS)
 		return false;
 
-	mutex_lock(&lock);
-	if (active_chain == chain)
+	mutex_lock(&ccd_ctx.lock);
+	if (ccd_ctx.active_board == board)
 		return true;
 
-	active_chain = chain;
-	chain_mask = 1 << active_chain;
-	return U1_tca9535->write(U1_tca9535, 0x02, ~chain_mask);
+	ccd_ctx.active_board = board;
+	ccd_ctx.board_mask = 1 << ccd_ctx.active_board;
+	return i2c_slave_write(&ccd_ctx.U1_tca9535, 0x02, ~ccd_ctx.board_mask);
 }
 
 static bool __ccd_board_selector_reset(uint8_t mask)
 {
-	if (!U1_tca9535->write(U1_tca9535, 0x03, mask))
+	if (!i2c_slave_write(&ccd_ctx.U1_tca9535, 0x03, mask))
 		return false;
 	cgsleep_ms(RESET_LOW_TIME_MS);
-	if (!U1_tca9535->write(U1_tca9535, 0x03, 0x00))
+	if (!i2c_slave_write(&ccd_ctx.U1_tca9535, 0x03, 0x00))
 		return false;
 	cgsleep_ms(RESET_HI_TIME_MS);
 	return true;
@@ -93,14 +96,14 @@ static bool __ccd_board_selector_reset(uint8_t mask)
 // we assume we are already holding the mutex
 static bool ccd_reset(void)
 {
-	return __ccd_board_selector_reset(chain_mask);
+	return __ccd_board_selector_reset(ccd_ctx.board_mask);
 }
 
 static bool ccd_reset_all(void)
 {
-	mutex_lock(&lock);
+	mutex_lock(&ccd_ctx.lock);
 	bool retval = __ccd_board_selector_reset(0xff & ~UNUSED_BITS);
-	mutex_unlock(&lock);
+	mutex_unlock(&ccd_ctx.lock);
 	return retval;
 }
 
@@ -111,7 +114,5 @@ static struct board_selector ccd_selector = {
 	.exit = ccd_exit,
 	.reset = ccd_reset,
 	.reset_all = ccd_reset_all,
-	/* don't have a temp sensor dedicated to chain */
-	.get_temp = dummy_get_temp,
+	.get_temp = dummy_u8,
 };
-
